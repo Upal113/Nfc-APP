4d643a6cc3f98ba90e08dd7f579f9ff8b2839f23
Upal <upalkundu287@gmail.com>
2023-03-24 11:13:31 -0700

    add

--- main.py
+++ main.py
@@ -48,9 +48,30 @@ def adddetails():
     
 @app.route('/view/<user_id>', methods=['GET'])
 def view(user_id):
-    data = ref.child(user_id).get()
-    message = "" + "/" + str(user_id)
-    return render_template('profilesetup.html', data=data, message=message)
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        password = request.form['password']
+        linkedin = request.form['linkedin']
+        product_image = request.files['image']
+        temp = tempfile.NamedTemporaryFile(delete=False)
+        product_image.save(temp.name)
+        bucket = storage.bucket(name='signupsyatem.appspot.com', app=db_app)
+        blob = bucket.blob(temp.name)
+        blob.upload_from_filename(temp.name)
+            # Opt : if you want to make public access from the URL
+        blob.make_public()
+        product_image = blob.public_url
+        ref.child(user_id).update({
+            'name': name,
+            'email': email,
+            'phone': phone,
+            'linkedin': linkedin,
+            'password': password,
+            'image': product_image
+        })
+        return redirect("/view/" + user_id)
 
 @app.route('/login', methods=['GET', 'POST'])   
 def find():
@@ -70,5 +91,32 @@ def find():
     if request.method == 'GET':
         return render_template('find.html')
 
+@app.route('/update/<user_id>', methods=['GET', 'POST'])
+def update(user_id):
+    name = str(secrets.token_hex(nbytes=16))
+    cred = credentials.Certificate("signupsyatem-e20be6a93767.json")
+    print(name)
+    db_app = initialize_app(credential=cred, name=name)
+    ref = db.reference("/users/nfc-app", url='https://signupsyatem-default-rtdb.firebaseio.com/')
+
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    if request.method == "GET":
+        return render_template('login.html')
+    if request.method == "POST":
+        name = str(secrets.token_hex(nbytes=16))
+        cred = credentials.Certificate("signupsyatem-e20be6a93767.json")
+        print(name)
+        db_app = initialize_app(credential=cred, name=name)
+        ref = db.reference("/users/nfc-app", url='https://signupsyatem-default-rtdb.firebaseio.com/')
+        email = request.form['email']
+        password = request.form['password']
+        data = ref.get()
+        for key in data.keys():
+            if data[key]['email'] == email and data[key]['password'] == password:
+                return redirect('/update/' + key)
+        else:
+            return {"error": "User not found"}
 if __name__ == "__main__":
     app.run(debug=True)
